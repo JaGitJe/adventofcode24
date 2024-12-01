23507d3e2139b8cd2fba673334801cfdce3ce2df
Jan <cmij2f38aj@mailbox.org>
2024-12-01 13:02:37 +0100

    Day 1 Part 1.

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,1 +1,20 @@
-module Day01 where
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
+
+module Day01 (sumDst, getPairs, getSortedLists) where
+
+import Data.List
+
+testInput :: String
+testInput = "14   12\n4   3\n2   2\n1   2\n7   9\n0   5"
+
+getSortedLists :: String -> ([Integer], [Integer])
+getSortedLists s = (sort l, sort r)
+ where
+  (l, r) = unzip $ map ((\[a, b] -> (read a, read b)) . words) . lines $ s
+
+getPairs :: ([Integer], [Integer]) -> [(Integer, Integer)]
+getPairs = uncurry zip
+
+sumDst :: [(Integer, Integer)] -> Integer
+sumDst = foldl (\s (a, b) -> s + abs (a - b)) 0
--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,15 @@
 module Main where
 
--- import Day01
+import Day01
 
-runDay :: (String, String -> Int) -> IO ()
+runDay :: (String, String -> Integer) -> IO ()
 runDay (path, f) = do
   udata <- readFile path
   let result = f udata
   print result
 
--- d01p1 :: (String, String -> Int)
--- d01p1 = ("data/d01p1.txt", f . g)
+d01p1 :: (String, String -> Integer)
+d01p1 = ("data/d01p1.txt", sumDst . getPairs . getSortedLists)
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
