75507bcc45e1334055d28ce84f170f512e8baee1
Jan <cmij2f38aj@mailbox.org>
2024-12-07 02:26:13 +0100

    Day 3 Part 1.

---
+++ src/Day03.hs
@@ -0,0 +1,32 @@
+module Day03 where
+
+import Text.Parsec
+
+testInput :: String
+testInput = "w()xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+\nmul(32,64]then(mul(11,8)mul(8,5))"
+
+numParser :: Parsec String () (Int, Int)
+numParser = do
+  _ <- string "mul("
+  a <- num
+  _ <- char ','
+  b <- num
+  _ <- char ')'
+  return (read a, read b)
+ where
+  num = many1 digit
+
+mulParser :: Parsec String () [(Int, Int)]
+mulParser = many1 (try numParser <|> junkParser)
+
+junkParser :: Parsec String () (Int, Int)
+junkParser = anyChar >> return (0, 0)
+
+parseMem :: String -> [(Int, Int)]
+parseMem s =
+  case parse mulParser "" s of
+    Left _ -> []
+    Right xs -> xs
+
+mul :: [(Int, Int)] -> Int
+mul = sum . map (uncurry (*))
--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,8 @@
 module Main where
 
 import Day01
-import Day02 (countSafe, damp, getReports, safe)
+import Day02
+import Day03
 
 runDay :: (String, String -> Int) -> IO ()
 runDay (path, f) = do
@@ -20,6 +21,9 @@ d02p1 = ("data/d02p1.txt", countSafe safe . getReports)
 
 d02p2 :: (String, String -> Int)
 d02p2 = ("data/d02p1.txt", countSafe damp . getReports)
+
+d03p1 :: (String, String -> Int)
+d03p1 = ("data/d03p1.txt", mul . parseMem)
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
